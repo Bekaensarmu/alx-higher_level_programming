c87db77573886c705899665e8f588173dd6148f4
Bekaensarmu <beekanensarmu@gmail.com>
2023-08-08 14:52:21 -0700

    10-add.py

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 # 10-add.py
 
+
 def add(a, b):
-    result = a + b
-    return result
-num1 = 5
-num2 = 7
-sum_result = add(num1, num2)
-print("Sum:", sum_result)
+    """Return the addition of a and b."""
+    return (a + b)
