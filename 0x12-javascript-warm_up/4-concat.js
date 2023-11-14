789d754afb968b61aea535a7e06f7a43a0913046
Beka Ensarmu <beekanensarmu@gmail.com>
2023-11-13 16:19:27 -0800

    print two argument passed

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// prints two arguments passed to it, in the following format: “ is ”
+
+const myArgs = process.argv.slice(2);
+if (myArgs[0] && myArgs[1]) {
+  console.log(`${myArgs[0]}` + ' is ' + `${myArgs[1]}`);
+} else if (myArgs[0] && !myArgs[1]) {
+  console.log(`${myArgs[0]}` + ' is ' + 'undefined');
+} else {
+  console.log('undefined is undefined');
+}
